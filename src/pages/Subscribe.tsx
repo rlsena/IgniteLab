7233383fb483793ea70168c4c4b198e4b0a9fc5f
Rodrigo Sena <rlsena@gmail.com>
2022-06-26 15:48:49 -0300

    Projeto Finalizado 2

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,7 +1,7 @@
 import { gql, useMutation} from "@apollo/client"
 import { useState, FormEvent } from "react"
 import { useNavigate } from "react-router-dom"
-import { Logo } from "./logo"
+import { Logo } from "../components/logo"
 
 const CREATE_SUBSCRIBER_MUTATION = gql`
 mutation CreateSubscriber($name: String!, $email:String!){
