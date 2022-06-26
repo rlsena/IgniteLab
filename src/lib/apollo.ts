a4da3d094c552ce9f1cc28edee828e7cd8b6a660
Rodrigo Sena <rlsena@gmail.com>
2022-06-26 16:54:09 -0300

    Projeto Final

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,6 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
     uri: import.meta.env.VITE_API_URL,
     headers: {
+        'acces-control-allow-origin':'*',
         'authorization':`Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
     },
     cache: new InMemoryCache()
